ec6292565b0fec979f52a8c02d2dd91d42a0dd22
Ahmet Efe <62066614+ahmetefe98@users.noreply.github.com>
2021-01-31 23:16:19 +0100

    Update analyse_pca.py

--- nearest_neighbors/comparison/analyse_pca.py
+++ nearest_neighbors/comparison/analyse_pca.py
@@ -11,10 +11,8 @@ with open('./pca128/acc.txt', 'r') as f:
     acc128 = json.load(f)
 
 print('pca32:')
-print('min: ', min(acc32))
 print('mean: ', statistics.mean(acc32))
 print('median: ', statistics.median(acc32))
-print('max: ', max(acc32))
 
 indexed = list(enumerate(acc32))
 top5 = sorted(indexed, key=operator.itemgetter(1))[:5]
@@ -23,10 +21,8 @@ print('top5 class: ', top5)
 print('worst12 class: ', worst12)
 
 print('pca64:')
-print('min: ', min(acc64))
 print('mean: ', statistics.mean(acc64))
 print('median: ', statistics.median(acc64))
-print('max: ', max(acc64))
 
 indexed = list(enumerate(acc64))
 top5 = sorted(indexed, key=operator.itemgetter(1))[:5]
@@ -35,10 +31,8 @@ print('top5 class: ', top5)
 print('worst12 class: ', worst12)
 
 print('pca128:')
-print('min: ', min(acc128))
 print('mean: ', statistics.mean(acc128))
 print('median: ', statistics.median(acc128))
-print('max: ', max(acc128))
 
 indexed = list(enumerate(acc128))
 top5 = sorted(indexed, key=operator.itemgetter(1))[:5]
